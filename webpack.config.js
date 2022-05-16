5032b9eb8a63f2140072378248de1c6447deacf4
AlexAndrianov <aliaksei.andryianau@smartpension.co.uk>
2022-05-16 15:26:15 +0200

    added configs for prod and dev environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,3 @@
-const path = require('path');
-
-module.exports = {
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js',
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'src'),
-        port: 3000,
-        open: true,
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    }
-};
+module.exports = process.env.NODE_ENV === 'development'
+? require('./webpack.config.dev')
+: require('./webpack.config.prod');
